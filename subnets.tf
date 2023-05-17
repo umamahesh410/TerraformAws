bbec5435c7891bb199cb0be4d73b571cd92c49df
umamahesh410 <92028946+umamahesh410@users.noreply.github.com>
2023-05-17 16:01:15 +0530

    Create subnets

---
+++ subnets.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                      = "public-us-east-1a"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
+
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                      = "public-us-east-1b"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+}
